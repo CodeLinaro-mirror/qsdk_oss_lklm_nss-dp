d75524bdae56d3e0162e91b6fd6493e968468623
Nanda Krishnan <quic_nandkris@quicinc.com>
2022-09-30 15:01:54 +0530

    [qca-nss-dp] Free irq fix in wifi load/unload
    Change-Id: Ie408e2d5ff896bd4cb178c40e14c8bab418af2bc
    Signed-off-by: Nanda Krishnan <quic_nandkris@quicinc.com>

--- hal/dp_ops/edma_dp/edma_v2/edma_ppeds.c
+++ hal/dp_ops/edma_dp/edma_v2/edma_ppeds.c
@@ -1056,7 +1056,7 @@ void edma_ppeds_inst_del(edma_ppeds_handle_t *ppeds_handle)
 
 	irq_clear_status_flags(ppeds_node->rxfill_intr, IRQ_DISABLE_UNLAZY);
 	free_irq(ppeds_node->rxfill_intr,
-			(void *)&ppeds_node->rxfill_intr);
+			(void *)&ppeds_node->rxfill_ring);
 	netif_napi_del(&ppeds_node->rxfill_ring.napi);
 
 	edma_ppeds_rx_fill_ring_free(&ppeds_node->rxfill_ring);
