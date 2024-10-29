09ebef3120509bc44c7435eb3032d9de4c1a3cd4
Sneha Maganahalli <quic_smaganah@quicinc.com>
2024-10-29 14:59:48 +0530

    [qca-nss-dp] Print the edma tx logs before clearing the error bits.
    Print the edma tx logs before clearing the error bits.
    
    Change-Id: If193249b1ab184c529662e9eb4a1a57c19607d71
    Signed-off-by: Sneha Maganahalli <quic_smaganah@quicinc.com>

--- hal/dp_ops/edma_dp/edma_v2/edma_tx.c
+++ hal/dp_ops/edma_dp/edma_v2/edma_tx.c
@@ -132,27 +132,26 @@ uint32_t edma_tx_complete(uint32_t work_to_do, struct edma_txcmpl_ring *txcmpl_r
 
 			txcmpl_errors = EDMA_TXCOMP_RING_ERROR_GET(txcmpl->word3);
 			if (unlikely(txcmpl_errors)) {
-				long bit_pos;
-
-
 				/*
 				 * Demux the txcmpl error type.
 				 * There can multiple txcmpl errors in the same descriptors.
 				 * Hence we need to check for all the set
 				 * bits instead of just the first one.
 				 */
+				long bit_pos;
 				u64_stats_update_begin(&txcmpl_stats->syncp);
 				bit_pos = __builtin_ffs(txcmpl_errors);
 				while (bit_pos) {
-					++txcmpl_stats->errors[bit_pos - 1];
-					txcmpl_errors = txcmpl_errors & ~(0x1 << (bit_pos - 1));
-					bit_pos = __builtin_ffs(txcmpl_errors);
 					if (EDMA_DEBUG_LEVEL > 2) {
 						if (net_ratelimit()) {
 							edma_info("Error 0x%0x observed in tx complete %d ring\n",
 									txcmpl_errors, txcmpl_ring->id);
 						}
 					}
+
+					++txcmpl_stats->errors[bit_pos - 1];
+					txcmpl_errors = txcmpl_errors & ~(0x1 << (bit_pos - 1));
+					bit_pos = __builtin_ffs(txcmpl_errors);
 				}
 				u64_stats_update_end(&txcmpl_stats->syncp);
 			}
