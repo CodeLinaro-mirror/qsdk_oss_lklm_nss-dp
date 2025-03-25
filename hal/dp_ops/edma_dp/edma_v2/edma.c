71a70bc97390766dd546c9bdfc724fcedbf95c0d
unnadixi <quic_unnadixi@quicinc.com>
2025-03-25 16:55:42 +0530

    [qca-nss-dp] Enabling Hairpin NATv6 with EDMA loopback ring
    Change-Id: Id2ca4cdcc21d1965c23c758d24a29985c5b2bb8c
    Signed-off-by: Unnati Dixit <quic_unnadixi@quicinc.com>
    (cherry picked from commit 3d8c349c93091dc8f992eff5261015d2812396c3)

--- hal/dp_ops/edma_dp/edma_v2/edma.c
+++ hal/dp_ops/edma_dp/edma_v2/edma.c
@@ -900,6 +900,10 @@ static int edma_of_get_pdata(struct resource *edma_res)
 		loopback_feature_type |= PPE_DRV_LOOPBACK_FEATURE_TYPE_GRETAP_MAPT;
 	}
 
+	if (edma_loopback_feature_type == PPE_DRV_LOOPBACK_FEATURE_TYPE_V6_HAIRPIN_NAT) {
+		loopback_feature_type |= PPE_DRV_LOOPBACK_FEATURE_TYPE_V6_HAIRPIN_NAT;
+	}
+
 	if (loopback_feature_type == 0) {
 		goto skip_loopback;
 	}
