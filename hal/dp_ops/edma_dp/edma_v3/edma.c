2a519f387c7299996a38db206a7e667803f15298
Sourav Poddar <souravp@qti.qualcomm.com>
2025-10-14 12:37:24 +0530

    [qca-nss-dp] Fix memory allocation
    Change-Id: I0c2e6350e8231f3acd9ce1736a9fc8a3ec17bd7a
    Signed-off-by: Sourav Poddar <souravp@qti.qualcomm.com>

--- hal/dp_ops/edma_dp/edma_v3/edma.c
+++ hal/dp_ops/edma_dp/edma_v3/edma.c
@@ -916,20 +916,20 @@ static int edma_of_get_pdata(struct resource *edma_res)
 		return -EINVAL;
 	}
 
-	edma_gbl_ctx.txdesc_loopback_ring_id_arr = kmalloc(GFP_KERNEL, sizeof(uint8_t) * edma_gbl_ctx.num_loopback_rings);
+	edma_gbl_ctx.txdesc_loopback_ring_id_arr = kmalloc(sizeof(uint8_t) * edma_gbl_ctx.num_loopback_rings, GFP_KERNEL);
 	if (!edma_gbl_ctx.txdesc_loopback_ring_id_arr) {
 		edma_err("Unable to allocate memory for txdesc loopback ring_id\n");
 		return -EINVAL;
 	}
 
-	edma_gbl_ctx.txcmpl_loopback_ring_id_arr = kmalloc(GFP_KERNEL, sizeof(uint8_t) * edma_gbl_ctx.num_loopback_rings);
+	edma_gbl_ctx.txcmpl_loopback_ring_id_arr = kmalloc(sizeof(uint8_t) * edma_gbl_ctx.num_loopback_rings, GFP_KERNEL);
 	if (!edma_gbl_ctx.txcmpl_loopback_ring_id_arr) {
 		edma_err("Unable to allocate memory for txcmpl loopback ring_id\n");
 		kfree(edma_gbl_ctx.txdesc_loopback_ring_id_arr);
 		return -EINVAL;
 	}
 
-	edma_gbl_ctx.rxdesc_loopback_ring_id_arr = kmalloc(GFP_KERNEL, sizeof(uint8_t) * edma_gbl_ctx.num_loopback_rings);
+	edma_gbl_ctx.rxdesc_loopback_ring_id_arr = kmalloc(sizeof(uint8_t) * edma_gbl_ctx.num_loopback_rings, GFP_KERNEL);
 	if (!edma_gbl_ctx.rxdesc_loopback_ring_id_arr) {
 		edma_err("Unable to allocate memory for rxdesc loopback ring_id\n");
 		kfree(edma_gbl_ctx.txdesc_loopback_ring_id_arr);
@@ -937,7 +937,7 @@ static int edma_of_get_pdata(struct resource *edma_res)
 		return -EINVAL;
 	}
 
-	edma_gbl_ctx.rxfill_loopback_ring_id_arr = kmalloc(GFP_KERNEL, sizeof(uint8_t) * edma_gbl_ctx.num_loopback_rings);
+	edma_gbl_ctx.rxfill_loopback_ring_id_arr = kmalloc(sizeof(uint8_t) * edma_gbl_ctx.num_loopback_rings, GFP_KERNEL);
 	if (!edma_gbl_ctx.rxfill_loopback_ring_id_arr) {
 		edma_err("Unable to allocate memory for rxfill loopback ring_id\n");
 		kfree(edma_gbl_ctx.rxdesc_loopback_ring_id_arr);
