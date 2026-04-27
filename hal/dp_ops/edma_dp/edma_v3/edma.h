1199c915da913e6021e51f6f943dfcd273929ac1
Sourav Poddar <souravp@qti.qualcomm.com>
2026-04-27 17:07:09 +0530

    [qca-nss-dp][WAR] Disable HW TSO because of HW bug
    Currently HW hangs after a certain throughput because of a bug in HW TSO
    Disable HW TSO for now till HW fix is available.
    
    Change-Id: I4c24937c788daaa502a4dcff600f43b8fb606316
    Signed-off-by: Sourav Poddar <souravp@qti.qualcomm.com>

--- hal/dp_ops/edma_dp/edma_v3/edma.h
+++ hal/dp_ops/edma_dp/edma_v3/edma.h
@@ -87,16 +87,12 @@
                                        | NETIF_F_SG \
                                        | NETIF_F_RXCSUM \
                                        | NETIF_F_HW_CSUM \
-                                       | NETIF_F_TSO \
-                                       | NETIF_F_TSO6 \
                                        | NETIF_F_GRO_HW
 #else
 #define EDMA_NETDEV_FEATURES		NETIF_F_FRAGLIST \
 					| NETIF_F_SG \
 					| NETIF_F_RXCSUM \
-					| NETIF_F_HW_CSUM \
-					| NETIF_F_TSO \
-					| NETIF_F_TSO6
+					| NETIF_F_HW_CSUM
 #endif
 
 #define EDMA_SWITCH_DEV_ID	0
