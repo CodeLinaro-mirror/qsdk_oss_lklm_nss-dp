eeb5b32dcc2ac1b277a7fef66820216cc21214f9
Manish Verma <maniverm@qti.qualcomm.com>
2026-05-03 15:16:14 +0530

    [qca-nss-dp] Set DDRQ group drop threshold value to 256 less than the shared limit
    Change-Id: Ia76369316990f31cc7a6202e6711b723cead0cf6
    Signed-off-by: Manish Verma <maniverm@qti.qualcomm.com>

--- hal/dp_ops/edma_dp/edma_v3/edma_ddrq.c
+++ hal/dp_ops/edma_dp/edma_v3/edma_ddrq.c
@@ -1811,11 +1811,12 @@ static int32_t edma_ddrq_get_def_cfg(edma_ddrq_cfg_t *ddrq_cfg)
 	 * Get DDRQ group default configurations
 	 */
 	edma_ddrq_grp_cfg_set_inval(&ddrq_grp_cfg->ddrq_grp_cfg);
-	ddrq_grp_cfg->ddrq_grp_cfg.ac_grp_dp_thrd = edma_ddrq_blk_num_map[ddrq_gbl_cfg->ddrq_blk_num_cfg];
 	ddrq_grp_cfg->ddrq_grp_cfg.ac_cfg_ac_en= edma_ddrq_grp_ac_en;
 	ddrq_grp_cfg->ddrq_grp_cfg.ac_cfg_color_aware = edma_ddrq_grp_color_aware;
 	ddrq_grp_cfg->ddrq_grp_cfg.ac_grp_gap_shrd_limit = EDMA_DDRQ_GET_SHARED_LIMIT(edma_ddrq_blk_num_map[ddrq_gbl_cfg->ddrq_blk_num_cfg], EDMA_DDRQ_QUEUE_PRE_ALLOC_LIMIT_MIN);
 	ddrq_grp_cfg->ddrq_grp_en_bm = edma_ddrq_grp_id_bm;
+	ddrq_grp_cfg->ddrq_grp_cfg.ac_grp_dp_thrd = ddrq_grp_cfg->ddrq_grp_cfg.ac_grp_gap_shrd_limit -
+					EDMA_DDRQ_AC_GRP_GAP_SHRD_LMT_DRP_THRES_VAL;
 
 	/*
 	 * Get DDRQ loopback default configurations
